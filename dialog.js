bc7c5df78aa3fa37f6eef274c356f7e04a984844
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-10-29 21:10:57 -0300

    Modal basic structure and initial logic implementation.

--- dialog.js
+++ dialog.js
@@ -1,18 +1,20 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    handleButton();
+  });
+
 const handleButton = () => {
     console.log("test");
-    const getById = (idName) => document.getgetElementById(idName);
+    const getById = (idName) => document.getElementById(idName);
 
     const getByClass = (className) => document.getElementsByClassName(className);
     
-    
-    
     const handleDialogClick = () => {
-         getById('dialog-btn-1').addEventListener('click', () => {
+
+        getById('dialog-btn-1').addEventListener('click', () => {
             console.log("Has clicked this.");
+            getById("confirmation-dialog-modal-wrapper").classList.remove("hide-modal");
          });
          console.log("Te3st");
     }
     handleDialogClick();
 }
-
-handleButton();
