471e9b86ffa11aaf6bcab88f9f41675a9d978301
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-10-29 21:40:00 -0300

    Code cleanup.

--- dialog.js
+++ dialog.js
@@ -6,10 +6,7 @@ const handleButton = () => {
 
     const getById = (idName) => document.getElementById(idName);
 
-    const getByClass = (className) => document.getElementsByClassName(className);
-
     const handleAcceptBtn = () => getById("confirmation-btn").addEventListener('click' , () => {
-        console.log("1");
         getById("confirmation-dialog-modal-wrapper").classList.add("hide-modal");
         getById("dialog-response").textContent =('You just clicked "Yes"');
     });
@@ -25,7 +22,6 @@ const handleButton = () => {
     }
     
     const handleDialogClick = () => {
-        console.log("2");
         getById('dialog-btn-1').addEventListener('click', () => {
             console.log("Has clicked this.");
             getById("confirmation-dialog-modal-wrapper").classList.remove("hide-modal");
