1e450179434aafef7bde28d8624dc0a58c6f967a
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-10-29 21:34:15 -0300

    Final code logic and css improvements.

--- dialog.js
+++ dialog.js
@@ -3,18 +3,36 @@ document.addEventListener("DOMContentLoaded", () =>{
   });
 
 const handleButton = () => {
-    console.log("test");
+
     const getById = (idName) => document.getElementById(idName);
 
     const getByClass = (className) => document.getElementsByClassName(className);
+
+    const handleAcceptBtn = () => getById("confirmation-btn").addEventListener('click' , () => {
+        console.log("1");
+        getById("confirmation-dialog-modal-wrapper").classList.add("hide-modal");
+        getById("dialog-response").textContent =('You just clicked "Yes"');
+    });
+
+    const handleCancelBtn = () => getById("cancel-btn").addEventListener('click' , () => {
+        getById("confirmation-dialog-modal-wrapper").classList.add("hide-modal");
+        getById("dialog-response").textContent =('You just clicked "Cancel"');
+    });
+
+    const cleanupBtns = () => {
+        getById("accept-btn").removeEventListener('click');
+        getById("cancel-btn").removeEventListener('click');
+    }
     
     const handleDialogClick = () => {
-
+        console.log("2");
         getById('dialog-btn-1').addEventListener('click', () => {
             console.log("Has clicked this.");
             getById("confirmation-dialog-modal-wrapper").classList.remove("hide-modal");
+            handleAcceptBtn();
+            handleCancelBtn();
+            cleanupBtns();
          });
-         console.log("Te3st");
     }
     handleDialogClick();
 }
